425aa3f5b7b29739f8c07fee8d6744849104f124
Spiro Kolokithas <spiro@whatifwe.com.au>
2015-06-07 11:52:48 +1000

    minor change to comment in plot 2 - Course Project 1

--- plot2.R
+++ plot2.R
@@ -11,7 +11,7 @@ subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 
-## file creation and plot
+## file creation and plot.
 png("plot2.png", width=480, height=480)
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
